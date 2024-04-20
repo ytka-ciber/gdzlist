7b39995fd0533108179e2b748716ad3f616cf4fa
ytka-ciber <gkarpov076@gmail.com>
2024-04-20 22:07:13 +0500

    Сылки теперь в бд

--- links/models.py
+++ links/models.py
@@ -1,2 +1,15 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
+
+class Link(models.Model):
+    name = models.CharField(max_length=100)
+    url = models.URLField(max_length=1000)
+    description = models.TextField(default='Нет описания')
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
 
--- links/views.py
+++ links/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
+from .models import Link
 
 
 def links_hub(request):
-    return render(request, 'links_hub.html')
+    links = Link.objects.all()
+    return render(request, 'links_hub.html', {'links': links})
 
 
 def main(request):
     if request.method == "POST":  # если пользователь отправил форму
         return redirect(links_hub)
+    print(request)
     return render(request, 'main.html')

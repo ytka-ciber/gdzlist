29fef5b9d34c66a4696a7b59ff44c4dd0371ca9a
ytka-ciber <gkarpov076@gmail.com>
2024-04-21 21:20:47 +0500

    21.04.2024

--- links/models.py
+++ links/models.py
@@ -2,14 +2,13 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+
+class Review(models.Model):
+    name = models.CharField(max_length=100, default='Имя ссылки')
+    review = models.CharField(max_length=1000, default='Отзыв')
+
 class Link(models.Model):
     name = models.CharField(max_length=100)
     url = models.URLField(max_length=1000)
     description = models.TextField(default='Нет описания')
-
-    def publish(self):
-        self.save()
-
-    def __str__(self):
-        return self.name
-
+    reviews = models.JSONField(default=[""])
--- links/views.py
+++ links/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render, redirect
-from .models import Link
+from .models import Link, Review
+from .forms import ReviewForm
 
 
 def links_hub(request):
+    if request.method == 'POST':
+        review_form = ReviewForm(request.POST)
+        if review_form.is_valid():
+            name = review_form.cleaned_data.get('name')
+            review = review_form.cleaned_data.get('review')
+            link = Link.objects.filter(name=name).first()
+            if link is not None:
+                link.reviews.append(review)
+                link.save()
+    else:
+        review_form = ReviewForm()
     links = Link.objects.all()
-    return render(request, 'links_hub.html', {'links': links})
+    reviews = Review.objects.all()
+    return render(request, 'links_hub.html', {
+        'links': links,
+        'review_form': review_form,
+        'reviews': reviews
+    })
 
 
 def main(request):
